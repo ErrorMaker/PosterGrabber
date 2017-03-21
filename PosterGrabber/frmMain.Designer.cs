ac8afc4530894a2625d1ddd3fa83650215fd670d
Scott Stamp <scottstamp851@gmail.com>
2017-03-21 14:17:27 -0230

    Initial commit.

---
+++ PosterGrabber/frmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace PosterGrabber
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInject = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInject
+            // 
+            this.txtInject.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInject.Location = new System.Drawing.Point(0, 0);
+            this.txtInject.Multiline = true;
+            this.txtInject.Name = "txtInject";
+            this.txtInject.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInject.Size = new System.Drawing.Size(365, 204);
+            this.txtInject.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 204);
+            this.Controls.Add(this.txtInject);
+            this.Name = "frmMain";
+            this.Text = "PosterGrabber";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInject;
+    }
+}
+
---
+++ PosterGrabber/frmMain.cs
@@ -0,0 +1,56 @@
+﻿using Sulakore.Habbo;
+using Sulakore.Modules;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tangine;
+using Sulakore.Communication;
+
+namespace PosterGrabber
+{
+    [Module("PosterGrabber", "Does some magical shit with posters d/w about it.")]
+    [Author("Scott Stamp", HabboName = "Iterator", Hotel = HHotel.Com)]
+    public partial class frmMain : ExtensionForm
+    {
+        private ushort movePoster = 0;
+        private ushort placePoster = 0;
+        public override bool IsRemoteModule => true;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            movePoster = Game.GetHeader("ac0a5da536aba32cd6e1ab52a7a1cbb5");
+            placePoster = Game.GetHeader("3c90715d21c127bcbce295c8c10cdc5c");
+
+            Triggers.OutAttach(movePoster, MovePoster);
+            Triggers.OutAttach(placePoster, PlacePoster);
+        }
+
+        private void MovePoster(DataInterceptedEventArgs e)
+        {
+            int posterId = e.Packet.ReadInteger();
+            string posterLoc = e.Packet.ReadString();
+
+            txtInject.Text += "{l}{u:" + movePoster + "}{i:" + posterId + "}{s:" + posterLoc + "}\r\n";
+        }
+
+        private void PlacePoster(DataInterceptedEventArgs e)
+        {
+            string posterData = e.Packet.ReadString();
+            string posterId = posterData.Substring(0, posterData.IndexOf(' '));
+            string posterLoc = posterData.Substring(posterData.IndexOf(' ') + 1, posterData.Length - (posterData.IndexOf(' ')+1));
+
+            txtInject.Text += "{l}{u:" + movePoster + "}{i:" + posterId + "}{s:" + posterLoc + "}\r\n";
+        }
+    }
+}
